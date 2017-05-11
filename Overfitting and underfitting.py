9c46b88c7db3cbac67dfc3251e12bfefc3cdb17c
cristianku <c.zantedeschi@gmail.com>
2017-05-11 10:00:50 +0200

    aa

---
+++ Overfitting and underfitting.py
@@ -0,0 +1,27 @@
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = ____
+
+    # Fit the classifier to the training data
+    ____
+
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(____, ____)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = ____(____, ____)
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
